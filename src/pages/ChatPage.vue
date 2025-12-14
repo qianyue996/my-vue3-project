1057b01e1a5fb7093f07edb89eb710b7cd8fb5cc
qianyue996 <3010875645@qq.com>
2025-12-14 16:24:29 +0800

    initial commit

---
+++ src/pages/ChatPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div><input type="text" v-model="messageInput" /></div>
+  <div><button @click="sendMessage">发送</button></div>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const api = '/steins/alg/common/chat'
+const messageInput = ref<string>('')
+const sendMessage = () => {
+  console.log('开始聊天')
+  fetch(api, {
+    method: 'POST',
+    headers: {
+      Accept: 'text/event-stream',
+    },
+    body: messageInput.value,
+  }).then((response) => {
+    if (response.status === 200) {
+      console.log('开始接收数据')
+    }
+  })
+}
+</script>
+<style scoped></style>
---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import ChatPage from '@/pages/ChatPage.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      component: ChatPage,
+      path: '/',
+    },
+  ],
+})
+
+export default router
